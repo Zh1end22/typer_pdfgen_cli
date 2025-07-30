b00d786d2a6beeb7b8b528da3207708d23d768fb
Antriksh Dubey <121251852+Zh1end22@users.noreply.github.com>
2025-07-30 19:18:49 +0530

    Update minio_utils.py

--- minio_utils.py
+++ minio_utils.py
@@ -3,7 +3,7 @@ import boto3
 def get_minio_client():
     return boto3.client(
         "s3",
-        endpoint_url="http://127.0.0.1:9000",  # Your MinIO server
+        endpoint_url="",  # put ur MinIO endpt api
         aws_access_key_id="minioadmin",
         aws_secret_access_key="minioadmin",
     )
